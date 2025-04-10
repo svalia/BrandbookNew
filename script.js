1bc2d46a817ac6fc967a7128539b36dcedcbb4c8
valiaworking <valiaworking@gmail.com>
2025-04-11 00:14:44 +0300

    Create base files

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const app = document.getElementById("app");
+    app.innerHTML = `
+        <div class="alert alert-info text-center">
+            Добро пожаловать в Brandbook Manager! Начните с загрузки данных.
+        </div>
+    `;
+});
